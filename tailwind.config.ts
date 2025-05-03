733dd5130b2c22c07a0a4d6cd19572bfaaac2e44
JoRap <111890209+jrapusas@users.noreply.github.com>
2025-05-03 12:10:45 +0800

    Inter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,10 @@ export default {
         lighter: '#eaeef2',
         dark: '#000',
         darker: '#111',
-      }
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
     },
   },
 
