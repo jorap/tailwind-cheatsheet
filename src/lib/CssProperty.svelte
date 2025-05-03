ceb35e086fe9a694a3f9d7ca69dbf38e2d2b69ed
JoRap <111890209+jrapusas@users.noreply.github.com>
2025-05-03 18:15:32 +0800

    Bug Fix

--- src/lib/CssProperty.svelte
+++ src/lib/CssProperty.svelte
@@ -5,7 +5,12 @@
 
 <details
     class="group border-t border-sky-500/20"
-    ontoggle={(e) => e.target.querySelector("a").classList.toggle("hidden")}
+    ontoggle={(e) => {
+        const target = e.target as HTMLDetailsElement;
+        const link = target.querySelector("a");
+        if (link) link.classList.toggle("hidden");
+        if (link) link.classList.toggle("flex");
+    }}
 >
     <summary
         class="cursor-pointer flex justify-between py-2 px-4 hover:bg-white dark:hover:bg-black group-open:bg-white dark:group-open:bg-black"
@@ -16,7 +21,7 @@
             <a
                 href={b.url}
                 target="_blank"
-                class="hidden flex items-center gap-1 text-xs font-semibold not-italic text-sky-500 border border-sky-500 px-1 rounded"
+                class="hidden items-center gap-1 text-xs font-semibold not-italic text-sky-500 border border-sky-500 px-1 rounded"
             >
                 <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128">
                     <path fill="#38bdf8" d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597c6.398-8.531 13.867-11.73 22.398-9.597c4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602q-9.6 12.803-22.399 9.602c-4.87-1.215-8.347-4.746-12.207-8.66c-6.27-6.367-13.53-13.738-29.394-13.738M32.004 64c-17.066 0-27.73 8.531-32 25.602Q9.603 76.799 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66c6.274 6.367 13.536 13.738 29.395 13.738c17.066 0 27.73-8.53 32-25.597q-9.6 12.797-22.399 9.597c-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64m0 0" />
